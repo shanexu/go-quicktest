97a7bb971bc1666fb431c4cfb8531f297ba54281
Xu Shane <xusheng0711@gmail.com>
2020-05-13 13:52:21 +0800

    list

--- cmd/remove-nth-node-from-end-of-list/main.go
+++ cmd/remove-nth-node-from-end-of-list/main.go
@@ -1,15 +1,50 @@
 package main
 
+import "fmt"
+
 // Definition for singly-linked list.
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
+func printListNode(head *ListNode) {
+	for h := head; h != nil; h = h.Next {
+		fmt.Println(h.Val)
+	}
+}
+
 func main() {
+	node := &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+			Next: &ListNode{
+				Val: 3,
+				Next: &ListNode{
+					Val: 4,
+				},
+			},
+		},
+	}
+	node = removeNthFromEnd(node, 3)
+	printListNode(node)
 
 }
 
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	return nil
+	a := head
+	m := 0
+	for b := head; b.Next != nil; b = b.Next {
+		if m == n {
+			a = a.Next
+		} else {
+			m++
+		}
+	}
+	if m == n-1 {
+		return a.Next
+	}
+	a.Next = a.Next.Next
+	return head
 }
