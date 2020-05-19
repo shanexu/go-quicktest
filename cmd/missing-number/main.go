72b63eb910b85c8d2d10eaec212399c75e76dddb
Xu Shane <xusheng0711@gmail.com>
2020-05-19 12:44:20 +0800

    missing number

--- cmd/missing-number/main.go
+++ cmd/missing-number/main.go
@@ -3,32 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(missingNumber([]int{0,1,2}))
+	fmt.Println(missingNumber([]int{0, 1, 2}))
 }
 
 func missingNumber(nums []int) int {
-	n := len(nums)
-	for i := 0; i < n; i++ {
-		nums[i] += 1
+	sum := 0
+	for i := 1; i <= len(nums); i++ {
+		sum += i
 	}
-	for i := 0; i < n; i++ {
-		if abs(nums[i])-1 < n {
-			nums[abs(nums[i])-1] = -nums[abs(nums[i])-1]
-		}
+	for _, t := range nums {
+		sum -= t
 	}
-	for i := 0; i < n; i++ {
-		if nums[i] > 0 {
-			return i
-		}
-	}
-	return n
-}
-
-func abs(v int) int {
-	switch {
-	case v < 0:
-		return -v
-	default:
-		return v
-	}
+	return sum
 }
