ce3e53be7011a697b440bfef3ff668c033dfc1ec
Shane Xu <xusheng0711@gmail.com>
2020-12-01 23:45:01 +0800

    rxgo

---
+++ cmd/rxgo_test/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/reactivex/rxgo/v2"
+)
+
+func main() {
+	ch := make(chan rxgo.Item, 100)
+	ob := rxgo.FromChannel(ch).Debounce(rxgo.WithDuration(time.Second * 5))
+	go func() {
+		ch := ob.Observe()
+		for {
+			select {
+			case i := <-ch:
+				fmt.Println(time.Now().Format(time.RFC3339), "->", i.V)
+			}
+		}
+	}()
+
+	go func() {
+		tick := time.NewTicker(time.Second * 10)
+		defer tick.Stop()
+		i := 0
+		for {
+			select {
+			case n := <-tick.C:
+				i++
+				ch <- rxgo.Of(i)
+				fmt.Println(n.Format(time.RFC3339), "<-", i)
+			}
+		}
+	}()
+
+	select {}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/shanexu/go-quicktest
 go 1.14
 
 require (
+	github.com/cenkalti/backoff/v4 v4.1.0 // indirect
+	github.com/reactivex/rxgo/v2 v2.4.0
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
 	go.uber.org/ratelimit v0.1.0
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 )
