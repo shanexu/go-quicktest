d5d5d7f2adcdc12ee4047c747b2e9152dbdd4987
Shane Xu <xusheng0711@gmail.com>
2023-10-23 14:45:28 +0800

    add chanel test

---
+++ cmd/chan_test/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Sender struct {
+	msgs chan string
+	ctx  context.Context
+}
+
+func (s *Sender) startSend() {
+
+}
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+	defer cancel()
+	msgs := make(chan string, 1024*10)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+	forloop:
+		for {
+			select {
+			case msg := <-msgs:
+				fmt.Println("send: ", msg)
+				time.Sleep(100 * time.Millisecond)
+			case <-ctx.Done():
+				close(msgs)
+				break forloop
+			}
+		}
+		for msg := range msgs {
+			fmt.Println("drain: ", msg)
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		ticker := time.NewTicker(time.Millisecond * 50)
+	forloop:
+		for {
+			select {
+			case t := <-ticker.C:
+				msgs <- fmt.Sprintf("%s", t)
+			case <-ctx.Done():
+				break forloop
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+}
